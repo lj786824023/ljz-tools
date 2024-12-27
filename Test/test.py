55e744510808c1f3433210897ff0bbe36ef2169b
lojn-win11 <786824023@qq.com>
2024-12-27 18:00:15 +0800

    commit

--- Test/test.py
+++ Test/test.py
@@ -1,21 +1,4 @@
-import dmapi
-
-# 连接数据库
-conn = dmapi.connect(host='localhost', port='5236', user='username', password='password', dbname='dbname')
-
-# 创建游标对象
-cursor = conn.cursor()
-
-# 执行SQL语句
-cursor.execute("SELECT * FROM table_name")
-
-# 获取结果
-result = cursor.fetchall()
-
-# 打印结果
-for row in result:
-    print(row)
-
-# 关闭游标和连接
-cursor.close()
-conn.close()
+a = [1]
+b = [2, 3, 4]
+print(a+b[1:])
+print(b)
